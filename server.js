375f10e8204721a996790f9c4576fa2a8b31c7d0
pranavchap <30655218+pranavchap@users.noreply.github.com>
2017-08-07 21:33:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/article-two', function (req,res){
     res.send(' Article two will be served here ');
 });
 app.get('/article-three' , function (req,res){
-    res.send(' Article three will be served here')
+    res.send(' Article three will be served here');
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
